0775ad040880406928031c04d28efc3052daa111
Benjamin-madrid <benjamin.madrid@ug.uchile.cl>
2025-09-21 01:32:00 -0300

    refactor: change the types to fit the database

--- frontend/src/types/models.ts
+++ frontend/src/types/models.ts
@@ -1,4 +1,21 @@
 export interface RoomFeatures {
     maxCapacity: number;
     hasProjector: boolean;
+    hasWhiteboard: boolean;
+    hasAudio: boolean;
+    hasVentilation: boolean;
+}
+
+export interface Room {
+    id: number;
+    name: string;
+    features: RoomFeatures;
+}
+
+export interface Reservation {
+    id: number;
+    salaId: number;
+    userId: number;
+    hora: string;
+    estado: "aceptada" | "pendiente" | "rechazada";
 }
