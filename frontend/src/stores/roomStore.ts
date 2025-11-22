fa253915a3b5254e5db5077aaf046652865fed0d
Benjamin-madrid <benjamin.madrid@ug.uchile.cl>
2025-11-22 04:24:03 -0300

    refactor: agregar mas funciones al store de rooms y cambiar otras

--- frontend/src/stores/roomStore.ts
+++ frontend/src/stores/roomStore.ts
@@ -5,12 +5,18 @@ type RoomState = {
     rooms: Room[];
     selectedRoom: Room | null;
     setRooms: (rooms: Room[]) => void;
-    selectRoom: (room: Room) => void;
+    selectRoom: (room: Room | null) => void;
+    createRoom: (room: Room) => void;
+    deleteRoom: (roomId: number) => void;
+    updateRoom: (room: Room) => void;
 }
 
 export const useRoomStore = create<RoomState>((set) => ({
     rooms: [],
     selectedRoom: null,
     setRooms: (roomsData: Room[]) => { set({ rooms: roomsData }) },
-    selectRoom: (room: Room) => set({ selectedRoom: room }),
+    selectRoom: (room: Room | null) => set({ selectedRoom: room }),
+    createRoom: (room: Room) => set((state) => ({ rooms: [...state.rooms, room] })),
+    deleteRoom: (roomId: number) => set((state) => ({ rooms: state.rooms.filter(room => room.id !== roomId) })),
+    updateRoom: (updatedRoom: Room) => set((state) => ({ rooms: state.rooms.map(room => room.id === updatedRoom.id ? updatedRoom : room)})),
 }));
